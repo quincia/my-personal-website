c1cd6c7bb45bd7c0a70c67bd2b3c29bc186f637e
Umberto Quintarelli <umberto.quintarelli@intesys.it>
2020-12-30 22:33:22 +0100

    New posts

--- assets/js/scripts.min.3ca782d9.js
+++
@@ -1,2 +0,0 @@
-/* My personal website v0.0.1 -  - Copyright 2020  */
-$(document).ready(function(){var a=document.querySelectorAll(".dropcap");window.Dropcap.layout(a,2);responsiveNav(".nav-collapse");$(".time").text(function(a,b){return Math.round(parseFloat(b))})});
